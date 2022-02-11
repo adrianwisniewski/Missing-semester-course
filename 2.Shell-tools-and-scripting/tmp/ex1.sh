0441b89b2d183a05c95bf104e37e1bcc9342c03c
wisniewski.adrian@live.com <wisniewski.adrian@live.com>
2022-02-11 17:51:37 +0100

    task2(marco program) works fine now

--- 2.Shell-tools-and-scripting/tmp/ex1.sh
+++ 2.Shell-tools-and-scripting/tmp/ex1.sh
@@ -1,10 +1,13 @@
-#!/bin/bash
-
+mcd () {
+    mkdir -p "$1"
+    cd "$1"
+}
 function marco(){
 currentDirectory=$(pwd)
 
 echo "Current directory is $currentDirectory"
 }
+
 function polo(){
     cd "$currentDirectory"
 }
